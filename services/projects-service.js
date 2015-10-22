28c93679f4618c80cdbbac7192643f6e440bdec7
Michael Stewart <michael_stewart+gitdev@vt.edu>
2015-10-22 15:52:08 -0400

    seems to fix the infinite digest problem. not sure i like the fix
    seems to fix the problem described at https://docs.angularjs.org/error/$rootScope/infdig?p0=10&p1=%5B%5D not sure this is the best fix, but it seems to prevent redundant queries to server

--- services/projects-service.js
+++ services/projects-service.js
@@ -5,17 +5,20 @@
   ProjectsService.$inject = ['$http'];
   function ProjectsService($http) {
     this.projects = [];
+    this.loading = false;
 
     this.loadProjects = function () {
       return $http.get('/projects')
         .then(function (allProjects) {
-          this.projects = allProjects.data;
+          this.projects = this.projects.concat(allProjects.data);
+          this.loading = false;
           // console.log(this.projects);
         }.bind(this));
     }
 
     this.getProjects = function () {
-      if (this.projects.length === 0) {
+      if (this.projects.length === 0 && !this.loading) {
+        this.loading = true;
         this.loadProjects();
       } else {
         // console.log('gopt else');
